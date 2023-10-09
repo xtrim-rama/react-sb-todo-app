a20b1e37fd9c51d56353b8d92cb591c6ba84beff
Rama Chandra Nayak <35654507+rama5789@users.noreply.github.com>
2023-10-09 16:59:48 +0530

    redux store configured

---
+++ frontend/web/todo-ui/src/store/index.js
@@ -0,0 +1,21 @@
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import { persistReducer, persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+const rootReducer = combineReducers({});
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
+});
+const persistor = persistStore(store);
+
+export { store, persistor };
